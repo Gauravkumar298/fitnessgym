4e691124887b5359d91c119729b6f7bc2fbf3bf3
Gauravkumar298 <107765300+Gauravkumar298@users.noreply.github.com>
2022-07-08 00:35:37 +0530

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,4 @@
+const a = document.querySelector('#blog')
+a.addEventListener('click', function () {
+    alert("Since This is a STARTUP...Join us and share your fitness journey....We will update the Blog....");
+})
---
+++ gallery.js
@@ -0,0 +1,4 @@
+const b = document.querySelector('#gallery')
+b.addEventListener('click', function () {
+    alert("As this is a STARTUP we will update soon (BHAI ANIMATION SIKH RAHA HOON...SIKH LUNGA TO DALUNGA)");
+})
